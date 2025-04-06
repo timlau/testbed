22f6aabdeacae447853a49d6923576e337ebda70
Tim Lauridsen <tla@rasmil.dk>
2025-04-06 09:43:14 +0200

    reset goal before adding to transaction

--- dnf5daemon/iteration/backend_test.py
+++ dnf5daemon/iteration/backend_test.py
@@ -18,36 +18,30 @@ def main():
     client.open_session()
     for i in range(number_interation):
         print(f"Interation : {i + 1}")
-        installed = list(
-            client.package_list_fd("*", package_attrs=["full_nevra"], scope="installed")
-        )
+        installed = list(client.package_list_fd("*", package_attrs=["full_nevra"], scope="installed"))
         logger.info(f"Number of installed packages : {len(installed)}")
         pkg = client.package_list_fd("0xffff")[0]
         nevra = pkg["nevra"]
         logger.info(f"--> Installing {nevra}")
         logger.debug(f"DBUS: {client.session_rpm.object_path}.install()")
+        client.session_goal.reset()
         client.session_rpm.install(dbus.Array([nevra]), dbus.Dictionary({}))
         rc = client.resolve()
         rc = client.do_transaction()
         logger.info(f"tranaction complete (rc={rc})")
-        # client.session_goal.reset()
         client.session_base.reset()
-        # sleep(1)
-        installed = list(
-            client.package_list_fd("*", package_attrs=["full_nevra"], scope="installed")
-        )
+        installed = list(client.package_list_fd("*", package_attrs=["full_nevra"], scope="installed"))
         logger.info(f"Number of installed packages : {len(installed)}")
         pkg = client.package_list_fd("0xffff")[0]
         nevra = pkg["nevra"]
         logger.info(f"--> Removing {nevra}")
         logger.debug(f"DBUS: {client.session_rpm.object_path}.install()")
+        client.session_goal.reset()
         client.session_rpm.remove(dbus.Array([nevra]), dbus.Dictionary({}))
         rc = client.resolve()
         rc = client.do_transaction()
         logger.info(f"tranaction complete (rc={rc})")
-        # client.session_goal.reset()
         client.session_base.reset()
-        # sleep(1)
     client.close_session()
 
 
